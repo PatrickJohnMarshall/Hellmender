579cf40a2f162ffe6f6abb48c4aa194e6f2c9fb6
Patrick Marshall <patrickmarshall113@gmail.com>
2022-06-12 16:20:57 -0400

    added monster orchestrator to game

--- src/start.ts
+++ src/start.ts
@@ -2,16 +2,30 @@ import PlayerAction from './player/action/PlayerAction';
 import PlayerLocation from './player/PlayerLocation';
 import formatLog from './util/formatLog';
 import buildLayout from './tower-layout/buildLayout';
+import generateMonsters from './monsters/generateMonsters';
+import { format } from 'prettier';
 
 export default async function () {
   formatLog('You put on your robe and wizard hat.');
   const startingRoom = buildLayout();
+
+  const monsters = generateMonsters();
 
   const playerLocation = new PlayerLocation(startingRoom);
   const playerAction = new PlayerAction(playerLocation);
 
   while (true) {
     playerLocation.describe();
+    describeAllMonstersAtLocation(
+      monsters.getMonstersForRoom(playerLocation.getID())
+    );
     await playerAction.action('What now?');
   }
+}
+
+function describeAllMonstersAtLocation(monsters: any[]) {
+  monsters.forEach((monster) => {
+    formatLog('---Danger---');
+    monster.describe();
+  });
 }
