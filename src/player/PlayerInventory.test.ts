b03e878a50ae15a895ccb9601363420fcee247ac
Patrick Marshall <patrickmarshall113@gmail.com>
2022-06-19 16:05:15 -0400

    added player inventory

---
+++ src/player/PlayerInventory.test.ts
@@ -0,0 +1,21 @@
+import PlayerInventory from './PlayerInventory';
+
+const mockItem = {
+  getID: () => 'Vorpal Fist',
+  attackStats: () => ({
+    attackBonus: 0,
+    damage: { min: 1, max: 5 },
+  }),
+  describe: () => '',
+};
+
+describe('PlayerInventory', () => {
+  test('adds a weapon', () => {
+    const playerInventory = new PlayerInventory();
+
+    playerInventory.addWeapons(mockItem);
+
+    const weaponResult = playerInventory.getWeapons();
+    expect(weaponResult.length).toBe(1);
+  });
+});
