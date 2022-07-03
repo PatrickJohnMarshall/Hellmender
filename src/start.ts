c0e3d46ad997cbb5ce399bc814074dd47702b0bc
Patrick Marshall <patrickmarshall113@gmail.com>
2022-07-03 15:12:52 -0400

    hooked up attack action

--- src/start.ts
+++ src/start.ts
@@ -1,8 +1,10 @@
 import PlayerAction from './player/action/PlayerAction';
 import PlayerLocation from './player/PlayerLocation';
+import PlayerInventory from './player/PlayerInventory';
 import formatLog from './util/formatLog';
 import buildLayout from './tower-layout/buildLayout';
 import generateMonsters from './monsters/generateMonsters';
+import Fist from './items/weapons/Fist';
 
 export default async function () {
   formatLog('You put on your robe and wizard hat.');
@@ -10,15 +12,18 @@ export default async function () {
 
   const monsters = generateMonsters();
 
+  const fist = new Fist();
+
   const playerLocation = new PlayerLocation(startingRoom);
-  const playerAction = new PlayerAction(playerLocation);
+  const playerInventory = new PlayerInventory(fist);
+  const playerAction = new PlayerAction(playerLocation, playerInventory);
 
   while (true) {
+    const monstersInRoom = monsters.getMonstersForRoom(playerLocation.getID());
+    const validMonsterIDs = monstersInRoom.map((monster) => monster.getID());
     playerLocation.describe();
-    describeAllMonstersAtLocation(
-      monsters.getMonstersForRoom(playerLocation.getID())
-    );
-    await playerAction.action('What now?');
+    describeAllMonstersAtLocation(monstersInRoom);
+    await playerAction.action('What now?', validMonsterIDs);
   }
 }
 
