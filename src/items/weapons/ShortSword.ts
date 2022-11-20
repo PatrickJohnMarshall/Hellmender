2c17f906e816d1a181f4fc6a6d36829f581c6cbc
Patrick Marshall <patrickmarshall113@gmail.com>
2022-11-20 17:02:32 -0500

    fixed playerinventory tests

---
+++ src/items/weapons/ShortSword.ts
@@ -0,0 +1,21 @@
+import AbstractWeapons from "../AbstractWeapons";
+import formatLog from "../../util/formatLog";
+
+class ShortSword extends AbstractWeapons {
+  constructor() {
+    const stats = {
+      attackBonus: 1,
+      damage: { min: 1, max: 6 },
+    };
+    super({ id: "shortsword", stats });
+  }
+  describe(): string {
+    return formatLog(["Its a short sword."]);
+  }
+
+  inLocationDescription(): string {
+    return formatLog(["A dull shortsword rests... somewhere."]);
+  }
+}
+
+export default ShortSword;
