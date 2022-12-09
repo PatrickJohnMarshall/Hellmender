ec8faa894aa4348062800f72045210e5745b7586
Patrick Marshall <patrickmarshall113@gmail.com>
2022-12-09 13:03:59 -0500

    implimented new action results using parser

--- src/util/process-input/parseText.ts
+++ src/util/process-input/parseText.ts
@@ -4,6 +4,7 @@ import {
   nounDictionary,
   prepositionDictionary,
   directionSynonyms,
+  commandSynonyms,
   directionAntonyms,
 } from "./dictionary";
 
@@ -23,18 +24,6 @@ export default function parseText(input: string): {
     return true;
   }
 
-  const words = input.toLowerCase().split(" ").filter(filterArticles);
-
-  const prepositions = words.filter((word) =>
-    prepositionDictionary.includes(word)
-  );
-
-  const nouns = words.filter((word) => nounDictionary.includes(word));
-
-  const command = words.filter((word) => verbDictionary.includes(word));
-
-  const direction = words.filter((word) => directionDictionary.includes(word));
-
   function findSubject(sentence) {
     let subject = [];
     for (let i = 1; i < sentence.length; i++) {
@@ -48,7 +37,17 @@ export default function parseText(input: string): {
     return subject;
   }
 
-  const subject = findSubject(words);
+  function findSynonym(inputArray) {
+    if (directionSynonyms.hasOwnProperty(inputArray[0])) {
+      return [directionSynonyms[inputArray[0]]];
+    }
+
+    if (commandSynonyms.hasOwnProperty(inputArray[0])) {
+      return [commandSynonyms[inputArray[0]]];
+    }
+
+    return inputArray;
+  }
 
   function checkIfDefined(wordArray: string[]): string[] | null {
     if (wordArray === undefined) {
@@ -62,14 +61,6 @@ export default function parseText(input: string): {
     return wordArray;
   }
 
-  function findSynonym(inputArray) {
-    if (directionSynonyms.hasOwnProperty(inputArray[0])) {
-      return [directionSynonyms[inputArray[0]]];
-    }
-
-    return inputArray;
-  }
-
   function findAntonym(wordStr) {
     if (directionAntonyms.hasOwnProperty(wordStr)) {
       return [directionAntonyms[wordStr]];
@@ -77,7 +68,31 @@ export default function parseText(input: string): {
     return [wordStr];
   }
 
+  const words = input.toLowerCase().split(" ").filter(filterArticles);
+
+  const prepositions = words.filter((word) =>
+    prepositionDictionary.includes(word)
+  );
+
+  const nouns = words.filter((word) => nounDictionary.includes(word));
+
+  const command = findSynonym(
+    words.filter((word) => verbDictionary.includes(word))
+  );
+
+  const direction = findSynonym(
+    words.filter((word) => directionDictionary.includes(word))
+  );
+
+  const subject = findSubject(words);
+
   function compileOutput() {
+    if (!command[0]) {
+      return {
+        command: ["INVALID"],
+      };
+    }
+
     if (command[0] === "quit") {
       return {
         command: checkIfDefined(command),
@@ -85,18 +100,17 @@ export default function parseText(input: string): {
     }
 
     if (command[0] === "move") {
-      console.log(findSynonym(direction));
       if (direction[0] === "back") {
         return {
           command: checkIfDefined(command),
-          direction: checkIfDefined(findSynonym(direction)),
+          direction: checkIfDefined(direction),
           findAntonym: findAntonym,
         };
       }
 
       return {
         command: checkIfDefined(command),
-        direction: checkIfDefined(findSynonym(direction)),
+        direction: checkIfDefined(direction),
       };
     }
 
