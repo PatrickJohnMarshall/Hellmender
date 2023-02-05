cd5a688582baf73df150d8b517f48af0c092b7be
Patrick Marshall <patrickmarshall113@gmail.com>
2023-02-05 12:40:54 -0500

    readied dialog for components

--- src/dialog/Dialog.test.ts
+++ src/dialog/Dialog.test.ts
@@ -13,28 +13,64 @@ type Dialog = (
   | { text: string }
   | { text: string; answer: { answText: string; next: string }[] }
   | { id: string; text: string; next: string }
+  | {
+      id: string;
+      text: string;
+      rep: { target: string; value: number };
+      next: string;
+    }
   | { id: string; text: string }
 )[];
 
 const mockDialog: Dialog = [
   { text: "Hello this is text." },
   { text: "Yep." },
-  { text: "Sure is." },
+  { text: "Sure is.", next: "init_q" },
+
+  { text: "I'm not real, I'm just a break between testing texts." },
 
   {
-    text: "Can you see me?",
+    id: "not_real",
+    text: "Just a fake set of questions to test if the step function skips to the directed 'next'.",
     answer: [
-      { answText: "We're shit out of luck, captain.", next: "answ_failure" },
-      { answText: "God I hope so.", next: "answ_success" },
-      { answText: "Wrong way.", next: "answ_not_real" },
+      { answText: "Option 1", next: "Option_1_result" },
+      { answText: "Option 2", next: "Option_2_result" },
     ],
   },
 
-  { id: "answ_failure", text: "Oh god no.", next: "answ_end" },
-  { id: "answ_success", text: "Thank god.", next: "answ_end" },
-  { id: "answ_not_real", text: "You fucked up.", next: "answ_end" },
+  {
+    id: "init_q",
+    text: "Can you see me?",
+    answer: [
+      {
+        answText: "We're shit out of luck, captain.",
+        next: "init_q_failure_b1",
+      },
+      { answText: "God I hope so.", next: "init_q_success" },
+      { answText: "Wrong way.", next: "init_q_not_real" },
+    ],
+  },
 
-  { id: "answ_end", text: "Fin." },
+  {
+    id: "init_q_failure_b1",
+    text: "Are you serious?",
+    answer: [
+      { answText: "Naw man. Get pranked.", next: "init_q_failure_b1_prank" },
+      { answText: "Deadly.", next: "init_q_failure_b1_real" },
+    ],
+  },
+
+  {
+    id: "init_q_failure_b1_real",
+    text: "Dear god.",
+    rep: { target: "grumpkin", value: -30 },
+    next: "init_q_end",
+  },
+  { id: "init_q_failure_b1_prank", text: "Funny.", next: "init_q_end" },
+  { id: "init_q_success", text: "Thank god.", next: "init_q_end" },
+  { id: "init_q_not_real", text: "You fucked up.", next: "init_q_end" },
+
+  { id: "init_q_end", text: "Fin." },
 ];
 
 describe("PlayerStats", () => {
@@ -66,7 +102,9 @@ describe("PlayerStats", () => {
   test("answers question", () => {
     const grumpkinDialog = new DialogBranch(mockDialog);
 
-    grumpkinDialog.step(3);
+    grumpkinDialog.step();
+    grumpkinDialog.step();
+    grumpkinDialog.step();
     grumpkinDialog.answer("God I hope so.");
 
     expect(grumpkinDialog.getCurDialog().text).toEqual("Thank god.");
@@ -75,10 +113,48 @@ describe("PlayerStats", () => {
   test("ends dialog", () => {
     const grumpkinDialog = new DialogBranch(mockDialog);
 
-    grumpkinDialog.step(3);
+    grumpkinDialog.step();
+    grumpkinDialog.step();
+    grumpkinDialog.step();
     grumpkinDialog.answer("God I hope so.");
     grumpkinDialog.step();
 
     expect(grumpkinDialog.getCurDialog().text).toEqual("Fin.");
+  });
+
+  test("navigates branches", () => {
+    const grumpkinDialog = new DialogBranch(mockDialog);
+
+    grumpkinDialog.step();
+    grumpkinDialog.step();
+    grumpkinDialog.step();
+    grumpkinDialog.answer("We're shit out of luck, captain.");
+    grumpkinDialog.answer("Naw man. Get pranked.");
+    grumpkinDialog.step();
+
+    expect(grumpkinDialog.getCurDialog().text).toEqual("Fin.");
+  });
+
+  test("change rep based off of answers", () => {
+    let answRes;
+
+    const mockGrumpState = {
+      grumpkin: 50,
+    };
+
+    const dialogState = new DialogState(mockGrumpState);
+    const grumpkinDialog = new DialogBranch(mockDialog);
+
+    grumpkinDialog.step();
+    grumpkinDialog.step();
+    grumpkinDialog.step();
+
+    grumpkinDialog.answer("We're shit out of luck, captain.");
+
+    answRes = grumpkinDialog.answer("Deadly.");
+
+    dialogState.changeState(answRes.target, answRes.value);
+
+    expect(dialogState.getStates().grumpkin).toEqual(20);
   });
 });
